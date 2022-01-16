d7f067feab809b27a99d225fd2177cb908ec1710
adarshguptacse18 <adarshgupta.cse18@itbhu.ac.in>
2022-01-16 21:40:45 +0530

    Added timer, calories and reps support

--- logic/templates/sketch.js
+++ logic/templates/sketch.js
@@ -4,27 +4,33 @@ let newPoints = [];
 let newAngle = 0;
 let repCount = 0;
 let dir = 0;
+let shouldTimerStart = false;
 const repCountElement = document.getElementsByName('repCount')[0];
+const caloriesElement = document.getElementsByName('calories')[0];
 const exerciseInfo = {
   bicepCurls: {
     index: [6, 8, 10],
     upperLimit: 160,
     lowerLimit: 50,
+    cal: 0.1,
   },
   squats: {
     index: [12, 14, 16],
     upperLimit: 170,
     lowerLimit: 50,
+    cal: 0.3,
   },
   pushUps: {
     index: [6, 8, 10],
     upperLimit: 160,
     lowerLimit: 80,
+    cal: 0.6,
   },
   crunches: {
     index: [6, 12, 14],
     upperLimit: 130,
     lowerLimit: 50,
+    cal: 0.25,
   },
 };
 
@@ -37,7 +43,7 @@ function find_angle(A,B,C) {
 }
 
 function setup() {
-  createCanvas(500, 500).parent("canvas");
+  createCanvas(640, 480).parent("canvas");
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelReady);
@@ -47,6 +53,8 @@ function setup() {
 
 function updateRepCount() {
     repCountElement.textContent = repCount;
+    const newCalories = Math.round(repCount * exerciseInfo[excerciseType].cal);
+    caloriesElement.textContent = newCalories;
 }
 let once = true;
 function gotPoses(poses) {
@@ -77,8 +85,14 @@ function gotPoses(poses) {
             updateRepCount();
         }
     }
+    if(newPoints.length == 3) {
+      for(let i = 0; i < 3; i++) {
+        newPoints[i].x = lerp(newPoints[i].x, points[i].x, 0.5);
+        newPoints[i].y = lerp(newPoints[i].y, points[i].y, 0.5);
+      }
+    }
+    else newPoints = points
     newAngle = angle;
-    newPoints = points;
   }
 }
 
@@ -89,6 +103,7 @@ function modelReady() {
 function draw() {
   image(video, 0, 0);
   if(newPoints.length == 3) {
+    shouldTimerStart = true;
 		fill(0, 255, 0);
     newPoints.forEach((p) => {
       circle(p.x, p.y, 30);
@@ -114,9 +129,9 @@ function draw() {
 
 try{
   let currentTime = Date.now();
-  const timerElement = document.getElementsByName('timer')[0];
-  
   setInterval(() => {
+    if(!shouldTimerStart) return;
+    const timerElement = document.getElementsByName('timer')[0];
     const distance = Date.now() - currentTime;
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
